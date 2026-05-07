085faf17385a62f9ce7b1c013633eb05dbcb9ab9
Paulo Wells <paulowells@gmail.com>
2026-05-06 21:20:26 -0300

    fix: remove console logs previously logging api key

--- src/authentication/index.ts
+++ src/authentication/index.ts
@@ -4,10 +4,6 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
 	const apiKey = req.header("x-api-key");
 
-	console.log("Received API key:", apiKey); // Log the received API key for debugging
-	console.log("Expected API key:", process.env.API_KEY); // Log the expected API key for debugging
-	console.log(apiKey === process.env.API_KEY); // Log the comparison result for debugging
-
 	if (!apiKey || apiKey !== process.env.API_KEY) {
 		return res.status(401).json({ error: "Unauthorized" });
 	}
